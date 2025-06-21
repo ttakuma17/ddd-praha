4954f53d2b711a7e397241f29f1afe4921ee225b
ttakuma17 <ttanimiya@gmail.com>
2025-06-21 09:12:45 +0900

    Otel対応の準備

---
+++ praha-web/src/main/java/com/ddd/praha/_config/OpenTelemetryAppenderInitializer.java
@@ -0,0 +1,19 @@
+package com.ddd.praha._config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenTelemetryAppenderInitializer implements InitializingBean {
+  private final OpenTelemetry openTelemetry;
+  OpenTelemetryAppenderInitializer(OpenTelemetry openTelemetry) {
+    this.openTelemetry = openTelemetry;
+  }
+
+  @Override
+  public void afterPropertiesSet() {
+    OpenTelemetryAppender.install(this.openTelemetry);
+  }
+}
